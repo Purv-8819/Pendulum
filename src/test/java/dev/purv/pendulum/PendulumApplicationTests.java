4d7a2262fc71ef5efa7867efdee9aff4fee4ecff
Purv-8819 <purvpatel905@gmail.com>
2024-12-17 18:45:54 -0600

    first commit

---
+++ src/test/java/dev/purv/pendulum/PendulumApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.purv.pendulum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PendulumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
