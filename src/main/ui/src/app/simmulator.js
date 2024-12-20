61053916f1bf68a88e456ef3bdbb443f1f37c547
Purv-8819 <purvpatel905@gmail.com>
2024-12-19 18:17:55 -0600

    Changing dynamic view

---
+++ src/main/ui/src/app/simmulator.js
@@ -0,0 +1,50 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import p5 from "p5";
+
+const sketch = (p) => {
+  // p is a reference to the p5 instance this sketch is attached to
+  p.setup = function () {
+    let canvas = p.createCanvas(p.windowWidth / 2, p.windowWidth / 2);
+    canvas.style("margin: 0px");
+  };
+
+  p.draw = function () {
+    p.background(120);
+
+    //Middle Line
+    p.fill(255, 0, 0);
+    p.rect(0, p.height / 2, p.width, 2);
+
+    //Carrying box
+    p.fill(0, 0, 255);
+    //X pos
+    var xPos = 0;
+    if (p.mouseX < 0) {
+      xPos = 0;
+    } else if (p.mouseX > p.width - 60) {
+      xPos = p.width - 60;
+    } else {
+      xPos = p.mouseX;
+    }
+    p.rect(xPos, p.height / 2 - 20, 60, 40);
+  };
+};
+
+export default function Simmulator() {
+  // create a reference to the container in which the p5 instance should place the canvas
+  const p5ContainerRef = useRef();
+
+  useEffect(() => {
+    // On component creation, instantiate a p5 object with the sketch and container reference
+    const p5Instance = new p5(sketch, p5ContainerRef.current);
+
+    // On component destruction, delete the p5 instance
+    return () => {
+      p5Instance.remove();
+    };
+  }, []);
+
+  return <div ref={p5ContainerRef} />;
+}
