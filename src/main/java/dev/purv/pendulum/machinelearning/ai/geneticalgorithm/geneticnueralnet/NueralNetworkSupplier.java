9d20fc7fc9102cbef0c0398a46c016258c956496
Purv-8819 <purvpatel905@gmail.com>
2025-01-14 21:02:37 -0600

    genetic Nueral network interfaces and classes

---
+++ src/main/java/dev/purv/pendulum/machinelearning/ai/geneticalgorithm/geneticnueralnet/NueralNetworkSupplier.java
@@ -0,0 +1,10 @@
+package dev.purv.pendulum.machinelearning.ai.geneticalgorithm.geneticnueralnet;
+
+import java.util.function.Supplier;
+
+import dev.purv.pendulum.machinelearning.ai.nueralnetwork.NueralNetwork;
+
+@FunctionalInterface
+public interface NueralNetworkSupplier extends Supplier<NueralNetwork>{
+
+}
