bc6b487615dd13669a9fa55055da5dc20365fbd2
Purv-8819 <purvpatel905@gmail.com>
2025-01-29 22:44:50 -0600

    Addes logic for cart and pendulum on backend

--- src/main/java/dev/purv/pendulum/simmulator/Cart.java
+++ src/main/java/dev/purv/pendulum/simmulator/Cart.java
@@ -1,13 +1,19 @@
 package dev.purv.pendulum.simmulator;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class Cart {
     //Attributes
+
     private double position;
     private double velocity;
+    @Getter
     private double acceleration;
     private double friction;
     private double size;
-    private Move move;
+    @Setter
+    private Move move = Move.NOTHING;
     private Pendulum attached;
     public enum Move {LEFT, RIGHT, BRAKE, NOTHING};
 
@@ -26,11 +32,71 @@ public class Cart {
         this.attached = p;
     }
 
-    public void setMove(Move m){
-        this.move = m;
+
+    /**
+     * Update the cart and attached pendulum
+     */
+    public void update(){
+        //Check the move and update correspondingly
+        switch (this.move){
+            case LEFT -> {
+                moveLeft();
+            }
+            case RIGHT -> {
+                moveRight();
+            }
+            case BRAKE -> {
+                brake();
+            }
+            case NOTHING -> {
+                nothing();
+            }
+        }
     }
 
-    public void update(){
+    /**
+     * Update the pendulum in moving left
+     */
+    private void moveLeft(){
+        this.acceleration -= 0.1;
+        updateSystem();
+    }
 
+    /**
+     * Update the pendulum in moving right
+     */
+    private void moveRight(){
+        this.acceleration += 0.1;
+        updateSystem();
+    }
+
+    /**
+     * Update the pendulum in breaking
+     */
+    private void brake(){
+        //Move the opposite way currently travelling
+        this.acceleration += getSign(this.velocity) * -0.1;
+        updateSystem();
+    }
+
+    private double getSign(double val){
+        return val < 0 ? -1 : 1;
+    }
+
+    /**
+     * Update the pendulum in doing nothing
+     */
+    private void nothing(){
+        this.acceleration = 0;
+        updateSystem();
+    }
+
+    /**
+     * Update the whole system from the current state
+     */
+    private void updateSystem(){
+        this.velocity += this.acceleration;
+        this.position += this.velocity;
+        this.attached.update();
     }
 }
---
+++ src/main/java/dev/purv/pendulum/simmulator/Pendulum.java
@@ -0,0 +1,43 @@
+package dev.purv.pendulum.simmulator;
+
+import lombok.Getter;
+
+public class Pendulum {
+    private final double length;
+    @Getter
+    private double angle;
+    private double angularVelocity;
+    private double angularAcceleration;
+    private static final double gravity = 9.8;
+    private final Cart cart;
+
+
+    public Pendulum(Cart c){
+        this.length = 5;
+        this.angle = 0;
+        this.angularVelocity = 0;
+        this.angularAcceleration = 0;
+        this.cart = c;
+        this.cart.attach(this);
+    }
+
+
+    public void update(){
+        //Calculate the angular acceleration component from the linear acceleration of the cart
+        double linearComponent = this.cart.getAcceleration() * (1/this.length) * Math.cos(this.angle);
+        //Calculate the angular acceleration component from the acceleration of gravity
+        double gravityComponent = Pendulum.gravity * Math.sin(this.angle) / this.length;
+
+        //Combine the two components
+        this.angularAcceleration = linearComponent + gravityComponent;
+        //Multiply by negative 1 so bottom is down
+        this.angularAcceleration *= -1;
+
+        //Add the acceleration to the velocity
+        this.angularVelocity += this.angularAcceleration;
+
+        //Add the velocity to angle
+        this.angle += this.angularVelocity;
+    }
+
+}
