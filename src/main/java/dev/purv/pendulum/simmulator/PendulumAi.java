31d30472edf0a3545dce5746f51c19ee77a8e954
Purv-8819 <purvpatel905@gmail.com>
2025-02-03 22:26:57 -0600

    uPdates to system and ai need to do score

--- src/main/java/dev/purv/pendulum/simmulator/PendulumAi.java
+++ src/main/java/dev/purv/pendulum/simmulator/PendulumAi.java
@@ -1,6 +1,7 @@
 package dev.purv.pendulum.simmulator;
 
 import dev.purv.pendulum.machinelearning.ai.nueralnetwork.NueralNetwork;
+import dev.purv.pendulum.machinelearning.linearalgebra.Vector;
 
 public class PendulumAi {
     private System system;
@@ -9,6 +10,47 @@ public class PendulumAi {
     public PendulumAi(NueralNetwork nn) {
         this.system = new System();
         this.nn = nn;
+    }
+
+    /**
+     * Start the simmulation for maxTime
+     * @param maxTime max number of ticks to run simmulation for
+     * @return score of the system
+     */
+    public double startPlaying(int maxTime){
+        while(system.getTickCounter() < maxTime){
+            getDirectionFromNueralNet();
+            system.tick();
+        }
+        return calcFitness();
+    }
+
+    private double calcFitness(){
+        return system.getScore();
+    }
+
+    /**
+     * Input the current state of the system and get the move from the nueral network
+     */
+    private void getDirectionFromNueralNet(){
+        Vector input = new Vector();
+        Vector result = nn.calcOutput(input);
+        switch (result.getBiggestIndex()){
+            case(0):
+            {
+                system.getCart().setMove(Cart.Move.LEFT);
+            }
+            case(1):{
+                system.getCart().setMove(Cart.Move.RIGHT);
+            }
+            case(2):{
+                system.getCart().setMove(Cart.Move.BRAKE);
+            }
+            default :{
+                system.getCart().setMove(Cart.Move.NOTHING);
+            }
+
+        }
     }
 
 
--- src/main/java/dev/purv/pendulum/simmulator/System.java
+++ src/main/java/dev/purv/pendulum/simmulator/System.java
@@ -4,12 +4,14 @@ import lombok.Getter;
 
 public class System {
     //Attributes
+    @Getter
     private final Cart cart;
     private final Pendulum pendulum;
     @Getter
     private int tickCounter;
     @Getter
     private int score;
+    private double cummulativeDistance;
 
     public System() {
         this.cart = new Cart();
@@ -25,5 +27,7 @@ public class System {
         if(Math.cos(this.pendulum.getAngle()) < 0){
             score ++;
         }
+        this.cummulativeDistance += Math.abs(this.cart.getPosition());
     }
+
 }
